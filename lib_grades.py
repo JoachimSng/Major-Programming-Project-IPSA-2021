c3f29bc306870a6e6bab9c2624487024c7c0bb1d
JoachimSng <96111294+JoachimSng@users.noreply.github.com>
2021-12-19 23:56:47 +0100

    Add files via upload

--- lib_grades.py
+++ lib_grades.py
@@ -4,37 +4,37 @@ from lib_common import read_csv_file, write_csv_file, append_csv_file
 def gradesManagement() :
     """
     This function performs
-    IN :
-    OUT :
+    IN : User selection
+    OUT : Desired function has been perforemed / altered CSV file
     """
     def addGrade(*gradeData) :
         """
         This function performs
-        IN :
-        OUT :
+        IN :gradeData List
+        OUT : Altered CSV file
         """
         filename1 = "courses.csv"
         filename2 = "grades.csv" 
         data1 = read_csv_file(filename1)
         data2 = read_csv_file(filename2)
-        id_ = len(data2)-1
-        row = list(gradeData)
-        row.insert(0, id_)
-        course = str(gradeData[2])
-        for i in range (0, len(data2)):
-            if course in data1:
-                append_csv_file(row, filename2)
-                print("Grade has been added")
+        id_ = len(data2)-1      #we generate an id
+        row = list(gradeData)       #create a row that we'll append to csv file
+        row.insert(0, id_)      #insert the id into our row
+        course = str(gradeData[2])  #extract the course from the gradeData list
+        for i in range (0, len(data2)): #make sure we scan the whole file
+            if course in data1:     #course has to be a valid one
+                append_csv_file(row, filename2) #we append our row
+                print("Grade has been added")   #confirmation for the user
                 return
             else:
-                print("This is not a valid course")
+                print("This is not a valid course")     #inform the user that the entered course doesn't exist
             return
     
     def modifyGrade(*gradeData) :
         """
         This function performs
-        IN :
-        OUT :
+        IN : gradeData 
+        OUT : Altered CSV file
         """
         filename1 = "courses.csv"
         filename2 = "grades.csv" 
@@ -45,19 +45,19 @@ def gradesManagement() :
         id_ = int(gradeData[0])
         if course in data1:
             for i in range (1, len(data2)):
-                if course == data2[i][3]:
-                    if id_ == data2[i][2]:
-                        grade = input("Whats the new grade?")
-                        row.insert(4, grade)
-                        data2[i][0] = row
-                        write_csv_file(data2, filename2)
-                        print("Grade has been modified")
+                if course == data2[i][3]:    #firstly we verify that the course has been attributed to a student
+                    if id_ == data2[i][2]:  #then we verify that someone's id has been linked to this course
+                        grade = float(input("Whats the new grade?")) #ask for the grade
+                        row.insert(4, grade) #modify the row to incorperate the grade
+                        data2[i][0] = row #we then write the row we've constructed into the extracted array
+                        write_csv_file(data2, filename2)    #we then re-write the csv fill with the altered array
+                        print("Grade has been modified") #if all conditions have been met then the user will get a success message
                         return
                     else:
-                        print("This id doesn't have an existing grade in this course,\n please add a grade for this person before modifying it")
+                        print("This id doesn't have an existing grade in this course,\n please add a grade for this person before modifying it") #the given id doesn't have a grade for this course
                         return
                 else:
-                    print("There is no student who has a grade in this course")
+                    print("There is no student who has a grade in this course") #grade for this course hasn't been added yet
                     return
         else:
             print("This is not a valid course")
@@ -66,14 +66,14 @@ def gradesManagement() :
     def deleteGrade(*gradeData) :
         """
         This function performs
-        IN :
-        OUT :
+        IN : Student Id and grade
+        OUT : altered csv file 
         """
         filename1 = "courses.csv"
         filename2 = "grades.csv" 
         data1 = read_csv_file(filename1)
         data2 = read_csv_file(filename2)
-        defaultRow = list()
+        defaultRow = list("")*4 #empty row to erase a line
         course = str(gradeData[2])
         id_ = int(gradeData[0])
         if course in data1:
@@ -85,13 +85,13 @@ def gradesManagement() :
                         print("Grade has been deleted")
                         return
                     else:
-                        print("This id doesn't have an existing grade in this course,\n please add a grade for this person before deleting it, or don't")
+                        print("This id doesn't have an existing grade in this course,\n please add a grade for this person before deleting it, or don't")   #student hasn't got a grade for this course
                         return
                 else:
-                    print("There is no student who has a grade in this course")
+                    print("There is no student who has a grade in this course")     #no-one has a grade in this course
                     return
         else:
-            print("This is not a valid course")
+            print("This is not a valid course")     #course isn't present in the valid course csv file
         return
     
     def displayGrades() :
--- lib_students.py
+++ lib_students.py
@@ -14,12 +14,12 @@ def studentsManagement() :
         OUT : Updated CSV file with new student info
         """
         filename = "students.csv" 
-        data = read_csv_file(filename)
-        id_ = len(data)
-        row = list(studentData)
-        row.insert(0, id_)
-        append_csv_file(row, filename)
-        print("Student has been added")
+        data = read_csv_file(filename)  # Data becomes an array which we'll modify then rewreite into the csv file
+        id_ = len(data)  #we geneerate an Id from the length of our csv file 
+        row = list(studentData)   #create a row from the values held by studentData
+        row.insert(0, id_) # insert in the first space of the list the generated id
+        append_csv_file(row, filename)  # we then make use of the append fucntion from the common library
+        print("Student has been added")  #confirmation message for the user
         return
     
     def modifyStudent(*studentData) :
@@ -28,19 +28,19 @@ def studentsManagement() :
         IN : studentData 
         OUT : Altered CSV file with modified student info for a specific ID
         """
-        id_ = int(input("Enter student's id"))
+        id_ = int(input("Enter student's id"))  #ask for an Id
         filename = "students.csv"
-        data = read_csv_file(filename)
-        row = list(studentData)       #creation of a list
-        row.insert(0, id_)
-        for i in range (1, len(data)):
-            if id_ == int(data[i][0]):
-                data[i] = row
-                write_csv_file(data, filename) 
-                print("Student has succesfully been modified")
+        data = read_csv_file(filename)  #open the file
+        row = list(studentData)       #creation of a list with the entered data
+        row.insert(0, id_)              #add the id to the list
+        for i in range (1, len(data)):      #making sure we scan the whole csv file
+            if id_ == int(data[i][0]):      #checking if the id has been attributed to a student
+                data[i] = row   #if it has then we change the line into the row we've built
+                write_csv_file(data, filename)  #the write function deletes whats in the csv file and rewrites everything with our desired array
+                print("Student has succesfully been modified")  #confirms to the user that we have succesfully changed the student's information
                 return
             else:
-                print("This student id hasn't been attributted ")
+                print("This student id hasn't been attributted ") #the if condition hasn't been fulfilled so we tell the user that the student has yet to exist in the csv file
                 return
         return
     
@@ -50,18 +50,18 @@ def studentsManagement() :
         IN : student ID
         OUT : Altered CSV file with deleted line
         """
-        id_ = int(student[0])
+        id_ = int(student[0])  #extract the id given by the user into a local variable
         filename = "students.csv"
         data = read_csv_file(filename)
-        row = [0, 0, 0, 0, 0, 0] #lists()
-        for i in range (1, len(data)+1):
-            if id_ == int(data[i][0]):
-                data[i] = row
-                write_csv_file(data, filename)
-                print("Student has been deleted")
+        defaultRow = list("")*4 #lists() create a default empty row that will allow us to delete a line of the array
+        for i in range (1, len(data)+1):  #making sure we scan the whole array
+            if id_ == int(data[i][0]):      #now we check if the id is present in the extracted array from the csv file
+                data[i] = defaultRow        #we delete the row where the id has been found
+                write_csv_file(data, filename) #rewrite the csv file with our modified array
+                print("Student has been deleted") #for the user
                 return
             else:
-                print("This student id hasn't been attributted ")
+                print("This student id hasn't been attributted ")   #same as before
                 return
         return
     
